a7cbdb57c3aef38a53736806c25328a710579039
Shruti <146953644+shrutiguptaa01@users.noreply.github.com>
2025-08-15 04:32:12 +0530

    commit2

--- backend/config/env.js
+++ backend/config/env.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 3000,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:5173'
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://intervue-polling-eight.vercel.app'
 };
 
 module.exports = config;
